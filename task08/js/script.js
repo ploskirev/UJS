aee8ea7080360de5d4c60dbcea7ee2538ee9330c
Denis <todenis@inbox.ru>
2020-01-25 21:48:56 +0300

    Добвлен таймер

--- task08/js/script.js
+++ task08/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', function() {
-  
+
   let tab = document.querySelectorAll('.info-header-tab'),
     info = document.querySelector('.info-header'),
     tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,4 +34,54 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
+
+  // Timer 
+
+  let deadline = '2020-01-26';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60) + '',
+      minutes = Math.floor((t / 1000 / 60) % 60) + '',
+      hours = Math.floor((t / (1000 * 60 * 60))) + '';
+      (hours.length == 1) && (hours = '0' + hours);
+      (minutes.length == 1) && (minutes = '0' + minutes);
+      (seconds.length == 1) && (seconds = '0' + seconds);
+      console.log(hours);
+      console.log(minutes);
+      console.log(seconds);
+      
+      
+
+    return {
+      'total': t,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+      }
+    }
+  }
+
+  setClock('timer', deadline);
 });
